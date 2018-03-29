6e67237c5967403716d99b67d66c0a078ca4bdbe
Maicol Aguilar Perez <ma.aguilar@udla.edu.co>
2018-03-28 23:48:23 -0500

    add style, and view login

---
+++ Proyecto/Proyecto/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class Login
+    {
+        public DataTable SELECT_USUARIO(string Username, string password)
+        {
+            try
+            {
+                return null;
+            }
+            catch (Exception io)
+            {
+                return null;
+            }
+        }
+    }
+}
