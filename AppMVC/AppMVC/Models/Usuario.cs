8768c5c168cdf2a52b861d9499e6677d5feaef10
Maicol Aguilar Perez <ma.aguilar@udla.edu.co>
2018-04-04 14:04:42 -0500

    add the big bang

--- AppMVC/AppMVC/Models/Usuario.cs
+++ AppMVC/AppMVC/Models/Usuario.cs
@@ -1,15 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AppMVC.AccesoDatos.Conection;
+using AppMVC.AccesoDatos.Interface;
+using System;
+using System.Data;
 
 namespace AppMVC.Models
 {
     public class Usuario
     {
+        /// <summary>
+        /// clase de conexion
+        /// </summary>
+        private IDatos dat = new Datos();
+
+        /// <summary>
+        /// metodos get and set
+        /// </summary>
         public string user_name { get; set; }
         public string user_pass { get; set; }
 
-
+        /// <summary>
+        /// método para consutlar un usuario
+        /// </summary>
+        /// <param name="usuario">conocido como nickname del usuario</param>
+        /// <param name="contrasena">contraseña pertenecinte al dicho nickname</param>
+        /// <returns></returns>
+        public DataRow Consoultar_usuario()
+        {
+            try
+            {
+                DataTable dta = new DataTable();
+                dta = dat.ConsultarDatos("CALL `consultar_usuario`('" + user_name + "', '" + user_pass+ "')");
+                if (dta.Rows.Count != 0)
+                {
+                    return dta.Rows[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
     }
 }
