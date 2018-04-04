a754b70593b6f6c368de4846cf8c088e9eee8a28
Maicol Aguilar Perez <ma.aguilar@udla.edu.co>
2018-04-04 12:24:23 -0500

    add proyect good

---
+++ AppMVC/AppMVC/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using AppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(Usuario obj)
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ AppMVC/AppMVC/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppMVC.Models
+{
+    public class Usuario
+    {
+        public string user_name { get; set; }
+        public string user_pass { get; set; }
+
+
+    }
+}
