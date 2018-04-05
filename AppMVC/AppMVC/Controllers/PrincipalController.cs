cd24994610511c92546bddb2893f551f27ae321f
Maicol Aguilar Perez <ma.aguilar@udla.edu.co>
2018-04-05 01:14:59 -0500

    Qparticipes

---
+++ AppMVC/AppMVC/Controllers/PrincipalController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMVC.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        // GET: Principal
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+
+    }
+}
--- AppMVC/AppMVC/Controllers/UsuarioController.cs
+++ AppMVC/AppMVC/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using AppMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,7 +19,18 @@ namespace AppMVC.Controllers
         [HttpPost]
         public ActionResult login(Usuario obj)
         {
-           
+            DataRow usrv = obj.Consoultar_usuario();
+            if (usrv != null)
+            {
+                Session["User"] = usrv;
+                //Session["Nombre"]=usrv.
+                Session["Menu"] = "<p>menu</p>";
+                Response.Redirect("PrivateViews/index.aspx");
+            }
+            else
+            {
+                ViewBag.Error = "<script>alert('Usuario o contraseña incorrecto');</script>";
+            }
             return View();
         }
     }
